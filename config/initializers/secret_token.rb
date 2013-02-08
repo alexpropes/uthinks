63bdb46c58713da03f5a6e157077060df837a8d8
Alex Propes <alex.propes@gmail.com>
2013-02-08 09:16:12 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uthinks::Application.config.secret_token = 'aa8e6e26ce3c72df0207a67fa32b9f0f36c76bf79586b4bb17c90783dfde7c28275c738ceb92c81fcd5476b56a93d328cf0ff7fcb04e88a0ded7d2ea1ba0a327'
